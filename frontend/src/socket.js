6435fbafe4a5e364177dd3127ba2fef6ef69607b
Orie Kanu <kanuorie3@gmail.com>
2026-04-29 15:20:22 +0100

    fix: socket connection config

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io(import.meta.env.VITE_API_URL, {
+  transports: ["websocket"], // more stable on Render
   withCredentials: true,
 });
-
 
 export default socket;
